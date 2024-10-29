c5fd179259fea554a59661c9759f854f12b12917
Mirelly3Ti <mihsato8@gmail.com>
2024-10-29 12:26:14 -0300

    finalizado

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@
 document.getElementById('decode-form').addEventListener('submit',
     function() {
         // Mostrar spinner
-        document.getElementById('loading_spinner').style.display = 'block';
+        document.getElementById('loading-spinner').style.display = 'block';
 
         //Esconder resultado
         document.getElementById('result').style.display = 'none';
